edc885c7e0fa7973742bf9e1aeaaf9849ce2f91f
Adwaith-Anand-sr <139732933+Adwaith-Anand-sr@users.noreply.github.com>
2024-05-14 20:29:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
    cors: {
-      origin: ["https://friendszone-vw8g.onrender.com"],
+      origin: ["https://friendszone-vw8g.onrender.com", "https://admin.socket.io"],
       credentials: true,
    }
 });
